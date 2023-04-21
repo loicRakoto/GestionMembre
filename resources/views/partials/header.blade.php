494a853f87d885f2dc6c67de8226e11c9b45c002
loicRakoto <rakotomandimby.loic@gmail.com>
2023-04-21 22:18:51 +0300

    Creation du controller Activite et quelque modification dans la vue principale

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark " style="background:#402c58;">
+<nav class="navbar navbar-expand-lg navbar-dark " style="background:#252129;">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">GESTION DES MEMBRES</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,10 +7,17 @@
       <div class="collapse navbar-collapse" id="navbarScroll">
         <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Index</a>
+            <a class="nav-link active" aria-current="page" href="{{ route('membre.index') }}">Membres</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Activites</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Participation</a>
           </li>
         </ul>
         <form class="d-flex">
+          
             <li class="nav-item dropdown dropstart" style="list-style: none">
                 <a style="color: white" class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <i class="fa-solid fa-right-to-bracket"></i>
